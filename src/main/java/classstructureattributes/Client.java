06e4afbb93e31d71e8dae6a02bb5b8140e11cba6
agotagitti <agota.subs@gmail.com>
2020-10-29 15:25:31 +0100

    Client

---
+++ src/main/java/classstructureattributes/Client.java
@@ -0,0 +1,9 @@
+package classstructureattributes;
+
+public class Client {
+
+    String name;
+    int year;
+    String address;
+
+}
---
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -0,0 +1,28 @@
+package classstructureattributes;
+
+import java.util.Scanner;
+
+public class ClientMain {
+
+    public static void main(String[] args) {
+        Client client = new Client();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("What is your name?");
+        client.name = scanner.nextLine();
+
+        System.out.println("Year of birth?");
+        client.year = scanner.nextInt();
+        client.address = scanner.nextLine();
+
+        System.out.println("Address?");
+        client.address = scanner.nextLine();
+
+        System.out.println("Name: " + client.name);
+        System.out.println("Year of birth: " + client.year);
+        System.out.println("Address: " + client.address);
+
+
+    }
+
+}
