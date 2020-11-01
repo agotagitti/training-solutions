984ede21c0035780f260a4fdf40334818e53b1d0
agotagitti <agota.subs@gmail.com>
2020-11-01 14:50:21 +0100

    Statements

---
+++ src/main/java/statements/statementsMain.java
@@ -0,0 +1,27 @@
+package statements;
+
+public class statementsMain {
+
+    public static void main(String[] args) {
+
+        int x = 5 + 6;
+
+        int y = 11 - x;
+
+        int z = 8;
+
+        boolean b = x > y;
+
+        System.out.println(b);
+
+        boolean c = b || z > 5;
+
+        System.out.println(c);
+
+        z++;
+
+        System.out.println(z);
+
+
+    }
+}
