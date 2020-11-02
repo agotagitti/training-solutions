3ccb94fa25f5e66d01d887d9c16e1b7e3cd0a843
agotagitti <agota.subs@gmail.com>
2020-11-02 17:15:22 +0100

    Greetings

---
+++ src/main/java/packages/greetings/Greeter.java
@@ -0,0 +1,9 @@
+package packages.greetings;
+
+public class Greeter {
+
+    public void sayHello() {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/main/java/packages/main/MainProgram.java
@@ -0,0 +1,14 @@
+package packages.main;
+
+import packages.greetings.Greeter;
+
+public class MainProgram {
+
+    public static void main(String[] args) {
+        Greeter greeter = new Greeter();
+
+        greeter.sayHello();
+
+    }
+
+}
