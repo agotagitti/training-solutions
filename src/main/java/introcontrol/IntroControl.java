08143922565c0726c0c36471051f8a60416ab9a7
agotagitti <agota.subs@gmail.com>
2020-11-03 19:06:48 +0100

    IntroControl

---
+++ src/main/java/introcontrol/IntroControl.java
@@ -0,0 +1,12 @@
+package introcontrol;
+
+public class IntroControl {
+
+    public int subtractTenIfGreaterThanTen(int number) {
+        if (number > 10) {
+            number -= 10;
+        }
+        return number;
+    }
+
+}
---
+++ src/main/java/introcontrol/IntroControlMain.java
@@ -0,0 +1,10 @@
+package introcontrol;
+
+public class IntroControlMain {
+
+    public static void main(String[] args) {
+        IntroControl num = new IntroControl();
+        System.out.println(num.subtractTenIfGreaterThanTen(25));
+    }
+
+}
