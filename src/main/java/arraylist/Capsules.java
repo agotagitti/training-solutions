cb11f17895f46e66649b3ad24390dc18f33b4ddd
agotagitti <agota.subs@gmail.com>
2020-11-05 10:22:44 +0100

    Capsules

---
+++ src/main/java/arraylist/Capsules.java
@@ -0,0 +1,52 @@
+package arraylist;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Capsules {
+
+    private List<String> colorList = new ArrayList<>();
+
+    public void addLast(String color) {
+        colorList.add(color);
+    }
+
+    public void addFirst(String color) {
+        colorList.add(0, color);
+    }
+
+    public void removeFirst() {
+        colorList.remove(0);
+    }
+
+    public void removeLast() {
+        colorList.remove(colorList.size() - 1);
+
+    }
+
+    public List<String> getColors() {
+       return colorList;
+    }
+
+    public static void main(String[] args) {
+        Capsules test = new Capsules();
+
+        test.addLast("blue");
+        test.addLast("green");
+
+        System.out.println(test.getColors());
+
+        test.addFirst("red");
+        test.addFirst("yellow");
+
+        System.out.println(test.getColors());
+
+        test.removeFirst();
+
+        System.out.println(test.getColors());
+
+        test.removeLast();
+
+        System.out.println(test.getColors());
+    }
+}
