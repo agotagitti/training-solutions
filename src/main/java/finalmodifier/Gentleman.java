f692d18ede25e7548cc0598ccd09be35ad7fb4d7
agotagitti <agota.subs@gmail.com>
2020-11-06 08:17:32 +0100

    Gentleman

---
+++ src/main/java/finalmodifier/Gentleman.java
@@ -0,0 +1,12 @@
+package finalmodifier;
+
+public class Gentleman {
+
+    static final String MESSAGE_PREFIX = "Hello ";
+
+    public String sayHello(String name) {
+        return (MESSAGE_PREFIX + name);
+    }
+
+
+}
