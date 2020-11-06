32f7f4bf3cf4aaff3d8422d93d635d13ccffb4db
agotagitti <agota.subs@gmail.com>
2020-11-06 13:12:34 +0100

    Languages

---
+++ src/main/java/week02/Languages.java
@@ -0,0 +1,20 @@
+package week02;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Languages {
+
+    public static void main(String[] args) {
+
+        List<String> test = Arrays.asList("Java", "Python", "JavaScript");
+
+        //5 karakternél hosszabbat kiírni
+
+        for (String x: test) {
+            if (x.length() > 5) {
+                System.out.println(x);
+            }
+        }
+    }
+}
