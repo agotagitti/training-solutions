5751bc4e98a377bf4db7fca0f436d6564bad8908
agotagitti <agota.subs@gmail.com>
2020-11-10 13:35:25 +0100

    Position

---
+++ src/main/java/week03/Position.java
@@ -0,0 +1,49 @@
+package week03;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Position {
+
+    private String name;
+    private int bonus;
+
+    public Position(String name, int bonus) {
+        this.name = name;
+        this.bonus = bonus;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBonus() {
+        return bonus;
+    }
+
+    @Override
+    public String toString() {
+        return "Position{" +
+                "name='" + name + '\'' +
+                ", bonus=" + bonus +
+                '}';
+    }
+
+
+
+    public static void main(String[] args) {
+
+        List<Position> positionList = new ArrayList<>();
+
+        positionList.add(new Position("CEO", 160_000));
+        positionList.add(new Position("CTO", 150_000));
+        positionList.add(new Position("CO", 200_000));
+
+        for (Position pos: positionList) {
+            if (pos.getBonus() > 150_000) {
+                System.out.println(pos);
+            }
+        }
+    }
+
+}
