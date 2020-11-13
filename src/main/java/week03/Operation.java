6fc8e85d86d0e084374a2e5ba1bd421fef20d569
agotagitti <agota.subs@gmail.com>
2020-11-13 14:41:58 +0100

    Operation

---
+++ src/main/java/week03/Operation.java
@@ -0,0 +1,27 @@
+package week03;
+
+import operators.Operators;
+
+public class Operation {
+
+    private int leftValue;
+    private int rightValue;
+
+    public Operation(String toConvert) {
+        //161+18
+        leftValue = Integer.parseInt(toConvert.substring(0, toConvert.indexOf("+")).trim());
+        rightValue = Integer.parseInt(toConvert.substring(toConvert.indexOf("+") + 1, toConvert.length()).trim());
+    }
+
+    public int getResult() {
+        return getLeftValue() + getRightValue();
+    }
+
+    public int getLeftValue() {
+        return leftValue;
+    }
+
+    public int getRightValue() {
+        return rightValue;
+    }
+}
---
+++ src/test/java/week03/OperationTest.java
@@ -0,0 +1,15 @@
+package week03;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class OperationTest {
+
+    @Test
+    public void testGetResult() {
+        Operation operation = new Operation("10+20");
+
+        assertEquals(30, operation.getResult());
+    }
+}
