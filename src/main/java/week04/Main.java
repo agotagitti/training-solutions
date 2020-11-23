f7f44924adf3ca6672715e12ee0e01c004a6b93d
agotagitti <agota.subs@gmail.com>
2020-11-23 11:17:35 +0100

    Main

--- src/main/java/week04/Main.java
+++ src/main/java/week04/Main.java
@@ -6,16 +6,16 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         Main main = new Main();
-       System.out.println(main.getIndexesOfChar("abcdefa", 'a'));
+        System.out.println(main.getIndexesOfChar("abcdefa", 'a'));
 
     }
 
     public List<Integer> getIndexesOfChar(String str, char c) {
         List<Integer> result = new ArrayList<>();
         for(int i = 0; i < str.length(); i++) {
-           if(str.charAt(i) == c) {
-               result.add(i);
-           }
+            if(str.charAt(i) == c) {
+                result.add(i);
+            }
         }
         return result;
     }
