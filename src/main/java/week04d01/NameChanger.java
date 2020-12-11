8c049dde255d5c5c5890a538d575f2fcd1276c7b
agotagitti <agota.subs@gmail.com>
2020-12-11 16:54:39 +0100

    NameChanger

--- src/main/java/week04d01/NameChanger.java
+++ src/main/java/week04d01/NameChanger.java
@@ -1,4 +1,4 @@
-package week04;
+package week04d01;
 
 public class NameChanger {
 
--- src/test/java/week04/NameChangerTest.java
+++ src/test/java/week04/NameChangerTest.java
@@ -1,6 +1,7 @@
 package week04;
 
 import org.junit.jupiter.api.Test;
+import week04d01.NameChanger;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
