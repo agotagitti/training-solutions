7ae7ba0781a9c7c04a67c9723326ca7f7b744fe9
agotagitti <agota.subs@gmail.com>
2020-12-28 13:20:39 +0100

    TypeWriterState

---
+++ src/main/java/statemachine/typewriter/TypeWriterState.java
@@ -0,0 +1,18 @@
+package statemachine.typewriter;
+
+public enum TypeWriterState {
+
+    UPPERCASE {
+        TypeWriterState next() {
+            return TypeWriterState.LOWERCASE;
+        }
+    },
+    LOWERCASE {
+        TypeWriterState next() {
+            return TypeWriterState.UPPERCASE;
+        }
+    };
+
+    abstract TypeWriterState next();
+
+}
