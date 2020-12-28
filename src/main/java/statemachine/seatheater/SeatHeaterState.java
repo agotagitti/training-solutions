42179a87a23323c5d94f13d553da2e437c8efb82
agotagitti <agota.subs@gmail.com>
2020-12-28 13:12:00 +0100

    SeatHeaterState

--- src/main/java/statemachine/seatheater/SeatHeaterState.java
+++ src/main/java/statemachine/seatheater/SeatHeaterState.java
@@ -4,31 +4,29 @@ public enum SeatHeaterState {
 
     OFF{
         @Override
-        public SeatHeaterState next() {
+        SeatHeaterState next() {
             return SeatHeaterState.THREE;
         }
     },
     ONE {
         @Override
-        public SeatHeaterState next() {
+        SeatHeaterState next() {
             return SeatHeaterState.OFF;
         }
     },
     TWO{
         @Override
-        public SeatHeaterState next() {
+        SeatHeaterState next() {
             return SeatHeaterState.ONE;
         }
     },
     THREE{
         @Override
-        public SeatHeaterState next() {
+        SeatHeaterState next() {
             return SeatHeaterState.TWO;
         }
     };
 
-    public SeatHeaterState next() {
-        return SeatHeaterState.OFF;
-    }
+    abstract SeatHeaterState next();
 
 }
