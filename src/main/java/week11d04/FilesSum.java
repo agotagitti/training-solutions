583f898bc2ab2e9037a4f031eee10a73c22600e5
agotagitti <agota.subs@gmail.com>
2021-01-21 17:38:42 +0100

    FileSum

--- src/main/java/week11d04/FilesSum.java
+++ src/main/java/week11d04/FilesSum.java
@@ -1,11 +1,31 @@
-//package week11d04;
-//
-//public class FilesSum {
-//
-////    public int sumNumber() {
-////
-////    }
-//
-//
-//
-//}
+package week11d04;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FilesSum {
+
+    public int sumNumber() {
+        int sum = 0;
+        for (int i = 0; i < 100; i++) {
+            Path path = Path.of("number" + String.format("%02d", i) + ".txt");
+            if (Files.isRegularFile(path)) {
+                try {
+                    sum += Integer.parseInt(Files.readString(path));
+                    System.out.println(path);
+                }
+                catch (IOException ioe) {
+                    throw new IllegalStateException("Cannot read file");
+                }
+            }
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        FilesSum filesSum = new FilesSum();
+        System.out.println(filesSum.sumNumber());
+    }
+
+}
