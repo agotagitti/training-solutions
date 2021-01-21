cebdd6684d96b911dfc9ad5c5504b96b79d5b211
agotagitti <agota.subs@gmail.com>
2021-01-21 10:01:50 +0100

    GuessTheNumber

--- src/main/java/week04d03/GuessTheNumber.java
+++ src/main/java/week04d03/GuessTheNumber.java
@@ -3,7 +3,7 @@ package week04d03;
 import java.util.Random;
 import java.util.Scanner;
 
-public class NumberGuesser {
+public class GuessTheNumber {
 
     public static void main(String[] args) {
         Random random = new Random();
