7425acba256bda505f925b26f9815e8145b1e044
agotagitti <agota.subs@gmail.com>
2021-01-24 17:08:23 +0100

    Ids

---
+++ src/main/java/ioreader/idread/IdManager.java
@@ -0,0 +1,29 @@
+package ioreader.idread;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class IdManager {
+
+    private List<String> ids = new ArrayList<>();
+
+    public void readIdsFromFile(String fileName) {
+        Path path = Path.of("src/main/resources/" + fileName);
+        try (BufferedReader bufferedReader = Files.newBufferedReader(path)){
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                ids.add(line);
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot read file");
+        }
+    }
+
+    public List<String> getIds() {
+        return new ArrayList<>(ids);
+    }
+}
