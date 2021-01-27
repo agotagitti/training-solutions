20d3ee52f95a76801844bb5ffab9e93e8a30b9d1
agotagitti <agota.subs@gmail.com>
2021-01-27 11:06:46 +0100

    EmployeeFileManager

---
+++ src/main/java/oizip/names/EmployeeFileManager.java
@@ -0,0 +1,24 @@
+package oizip.names;
+
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+public class EmployeeFileManager {
+
+    public void saveEmployees(Path path, List<String> employees) {
+        try (ZipOutputStream zos = new ZipOutputStream(new BufferedOutputStream(Files.newOutputStream(path)))) {
+            zos.putNextEntry(new ZipEntry("names.dat"));
+            for (String actual: employees) {
+                zos.write(actual.getBytes());
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException("Cannot write file", e);
+        }
+    }
+
+}
---
+++ src/test/java/oizip/names/EmployeeFileManagerTest.java
@@ -0,0 +1,47 @@
+package iozip.names;
+
+
+import oizip.names.EmployeeFileManager;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+
+public class EmployeeFileManagerTest {
+
+    @TempDir
+    public Path folder;
+
+    @Test
+    public void testSaveEmployees() throws IOException {
+        List<String> employees = List.of("John Doe", "Jack Doe", "Jane Doe");
+        Path path = Files.createFile(folder.resolve("employees.zip"));
+
+        EmployeeFileManager manager = new EmployeeFileManager();
+
+        manager.saveEmployees(path, employees);
+
+        byte[] saved = readEmployees(path);
+        byte[] asserted = "John DoeJack DoeJane Doe".getBytes();
+
+        assertArrayEquals(asserted, saved);
+    }
+
+    private byte[] readEmployees(Path path) throws IOException {
+        try (ZipFile zf = new ZipFile(path.toFile())) {
+            ZipEntry ze = zf.getEntry("names.dat");
+            try (InputStream is = zf.getInputStream(ze)) {
+                return is.readAllBytes();
+            }
+        }
+    }
+}
