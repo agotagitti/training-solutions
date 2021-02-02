a7920be2a568758382d540a9839a9a8716c95a9b
agotagitti <agota.subs@gmail.com>
2021-02-02 11:30:51 +0100

    Draw

--- src/main/java/collectionslist/collectionslinkedlist/Draw.java
+++ src/main/java/collectionslist/collectionslinkedlist/Draw.java
@@ -9,7 +9,7 @@ public class Draw {
         if (drawCount >= maxNumber) {
             throw new IllegalArgumentException("drawCount must be less then 8!");
         }
-        Queue<Integer> queue = new LinkedList<>(shuffle(numbersToDraw(maxNumber)));
+        Queue<Integer> queue = shuffle(numbersToDraw(maxNumber));
         Set<Integer> winnerNumbers = new TreeSet<>();
         for (int i = 0; i < 5; i++) {
             winnerNumbers.add(queue.poll());
@@ -17,15 +17,15 @@ public class Draw {
         return winnerNumbers;
     }
 
-    private List<Integer> numbersToDraw (int maxNumber) {
-        List<Integer> possibleNumbers = new LinkedList<>();
+    private LinkedList<Integer> numbersToDraw (int maxNumber) {
+        LinkedList<Integer> possibleNumbers = new LinkedList<>();
         for (int i = 1; i <= maxNumber; i++) {
             possibleNumbers.add(i);
         }
         return possibleNumbers;
     }
 
-    private List<Integer> shuffle(List<Integer> balls) {
+    private LinkedList<Integer> shuffle(LinkedList<Integer> balls) {
         Collections.shuffle(balls);
         return balls;
     }
