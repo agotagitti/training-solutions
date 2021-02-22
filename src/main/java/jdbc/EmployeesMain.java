fa6e863e65d82faebc94239657e89c5d25e97d6a
agotagitti <agota.subs@gmail.com>
2021-02-22 17:44:21 +0100

    test

---
+++ src/main/java/jdbc/EmployeesMain.java
@@ -0,0 +1,33 @@
+package jdbc;
+
+import org.mariadb.jdbc.MariaDbDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class EmployeesMain {
+
+    public static void main(String[] args) {
+        MariaDbDataSource mDbDataSource;
+        try {
+            mDbDataSource = new MariaDbDataSource();
+            mDbDataSource.setUrl("jdbc:mariadb://localhost:3306/employees?useUnicode=true");
+            mDbDataSource.setUser("employees");
+            mDbDataSource.setPassword("employees");
+        }
+        catch (SQLException se) {
+            throw new IllegalStateException("Cannot create data source", se);
+        }
+
+        try (Connection cnx = mDbDataSource.getConnection();
+            PreparedStatement pstmt = cnx.prepareStatement("insert into employees(emp_name) values (?)")) {
+            pstmt.setString(1, "Jill Doe");
+            pstmt.execute();
+        }
+        catch (SQLException se2) {
+            throw new IllegalStateException("Cannot connect data source", se2);
+        }
+    }
+}
